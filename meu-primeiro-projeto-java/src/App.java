23b3065f22833f3854d0f4740c63b087aad5147d
Vitor-dez <vvitorsilva37@gmail.com>
2025-02-11 00:25:49 -0300

    comit-class-app

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Eu Vou Passar no estágio");
+        System.out.println("Eu Vou Passar no estágio !");
     }
 }
